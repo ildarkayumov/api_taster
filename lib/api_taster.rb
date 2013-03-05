d584411916d09bc5f218714955154c6ab9a46a7c
ildarkayumov <darik4308@gmail.com>
2013-03-05 15:52:53 +0400

    Add route collector to ApiTaster

--- lib/api_taster.rb
+++ lib/api_taster.rb
@@ -6,6 +6,7 @@ require 'api_taster/engine'
 require 'api_taster/route'
 require 'api_taster/mapper'
 require 'api_taster/form_builder'
+require 'api_taster/route_collector'
 
 module ApiTaster
   mattr_accessor :global_params
